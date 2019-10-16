e70d3405a73898549c84473e58b44e47974a0be1
filippmark <lenovofm12@outlook.com>
2019-10-16 17:17:37 +0300

    test classs added

---
+++ FakerTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FakerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
